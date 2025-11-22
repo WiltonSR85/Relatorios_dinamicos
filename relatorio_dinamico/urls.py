fd67bdf65bc8b1e25831784e58aeeacff3031c41
natanfrota <natanfrota31@gmail.com>
2025-11-22 01:04:01 -0300

    feat: altera url

--- relatorio_dinamico/urls.py
+++ relatorio_dinamico/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.editor, name='editor'),
+    path('', views.index, name='index'),
+    path('editor/', views.editor, name='editor'),
     path('gerar_pdf/', views.gerar_pdf, name='gerar_pdf'),
-    path('index/', views.index, name='index'),
 ]
